4178c04aa902c0ce7988701403fc085f2e2fd0cc
Rafael Issao <rafael.issao@gmail.com>
2023-09-22 22:56:30 -0300

    Testing adoc instead of md

--- README.md
+++
@@ -1,2 +0,0 @@
-# solid-examples
-Example of SOLID principles
---
+++ app/src/main/java/br/com/meaxuda/examples/singleresponsibility/SingleResponsibilityPrinciple.java
@@ -0,0 +1,13 @@
+package br.com.meaxuda.examples.singleresponsibility;
+
+/**
+ * Origem: Robert C. Martin
+ * <p>
+ * Fonte: <a href="https://blog.cleancoder.com/uncle-bob/2014/05/08/SingleReponsibilityPrinciple.html">...</a>
+ * <p>
+ * Definição:
+ *    - Deixe junto (ou próximo) as coisas que mudam pela mesma razão
+ *    - Deixe separado (ou isolado) as coisas que mudam por razões diferentes
+ */
+public class SingleResponsibilityPrinciple {
+}
